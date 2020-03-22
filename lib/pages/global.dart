0df6e46af47547062eead40ce3e9ee65e9001c1b
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2020-03-23 04:04:00 +0700

    Temporary List of Countries

--- lib/pages/global.dart
+++ lib/pages/global.dart
@@ -3,15 +3,21 @@ import 'package:http/http.dart' as http;
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'dart:async';
 import 'dart:convert';
+import 'package:dropdownfield/dropdownfield.dart';
 
 class GlobalScreen extends StatefulWidget {
   @override
   _GlobalScreenState createState() => _GlobalScreenState();
 }
 
-Future<CovidData> fetchCovidData() async {
+Future<CovidData> fetchCovidData(country) async {
+  String baseUrl = 'https://covid19.mathdro.id/api';
+  String url = baseUrl;
+  if(country != 'All'){
+    url = baseUrl + '/countries/' + country;
+  }
   final response =
-  await http.get('https://covid19.mathdro.id/api');
+  await http.get(url);
 
   if(response.statusCode == 200){
     return CovidData.fromJson(json.decode(response.body));
@@ -48,14 +54,58 @@ class CovidData {
   }
 }
 
+//class Countries{
+//  String name;
+//  String iso;
+//
+//  Countries(this.name, this.iso);
+//
+////  @override
+////  String toString() {
+////    return '{ ${this.name}, ${this.iso} }';
+////  }
+//  Map<String, dynamic> toJson() => {
+//    "name": name,
+//    "iso": iso,
+//  };
+//}
 
 class _GlobalScreenState extends State<GlobalScreen> {
   Future<CovidData> futureCovidData;
 
+  String _baseCountriesUrl = 'https://covid19.mathdro.id/api/countries';
+  String _valCountries = 'All';
+//  List<dynamic> _dataCountries = List();
+  List<dynamic> _dataCountries = [
+    "All",
+    "Indonesia",
+    "Malaysia",
+    "Thailand",
+    "Singapore",
+    "Vietnam",
+    "Hongkong",
+    "China",
+    "Iran",
+    "Italy",
+  ];
+
+//  void getCountries() async {
+//    final response = await http.get(_baseCountriesUrl);
+//    Map<String, dynamic> mapData = jsonDecode(response.body);
+//    var listData = [];
+//    mapData['countries'].forEach((k, v) => listData.add(Countries(k, v)));
+//
+//    setState(() {
+//      _dataCountries = listData;
+//    });
+////    debugPrint(listData);
+//  }
+
   @override
   void initState() {
     super.initState();
-    futureCovidData = fetchCovidData();
+    futureCovidData = fetchCovidData(_valCountries);
+//    getCountries();
   }
 
   @override
@@ -103,11 +153,74 @@ class _GlobalScreenState extends State<GlobalScreen> {
                       padding: const EdgeInsets.all(8.0),
                       child: ListView(
                         children: <Widget>[
+                          DropdownButton(
+                            hint: Text("Select Countries"),
+                            value: _valCountries,
+                            items: _dataCountries.map((item){
+                              return DropdownMenuItem(
+                                child: Text(item),
+                                value: item,
+                              );
+                            }).toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                _valCountries = value;
+                                futureCovidData = fetchCovidData(_valCountries);
+                              });
+                            },
+                          ),
                           ListTile(
                             leading: Icon(Icons.info, color: Colors.blueAccent,),
-                            title: Text("Global Info"),
+                            title: Text("Global Info: $_valCountries"),
                             subtitle:
                             Text("Here update on global Pandemic of COVID19. Data updated at " +snapshot.data.lastUpdate),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Container(
+                                height: 30.0,
+                                width: 100.0,
+                                color: Colors.transparent,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.redAccent,
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                  ),
+                                  child: new Center(
+                                    child: new Text(snapshot.data.deaths.toString(), style: TextStyle(fontSize: 22, color: Colors.white), textAlign: TextAlign.center,),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 30.0,
+                                width: 100.0,
+                                color: Colors.transparent,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.greenAccent,
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                  ),
+                                  child: new Center(
+                                    child: new Text(snapshot.data.recovered.toString(), style: TextStyle(fontSize: 22, color: Colors.white), textAlign: TextAlign.center,),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 30.0,
+                                width: 100.0,
+                                color: Colors.transparent,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.yellowAccent,
+                                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                  ),
+                                  child: new Center(
+                                    child: new Text(snapshot.data.confirmed.toString(), style: TextStyle(fontSize: 22, color: Colors.black), textAlign: TextAlign.center,),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                           chartWidget,
                           ListTile(
